42826e7a9dd740d534457aeb29da3741d0c797f9
Volkney <jorge.24.co@gmail.com>
2023-02-07 15:16:25 -0600

    created new function and eventlistenener to create new grids

--- logic.js
+++ logic.js
@@ -1,29 +1,47 @@
 var childGrid = document.getElementById('child-grid');
 const value = document.getElementById('grid-value');
 const input = document.getElementById('grid-range');
-
-/* value for button */
-
+const valueToString = Number(value.textContent);
+input.value= 16;
 value.textContent = input.value;
+let totalNumberOfBoxes = input.value * input.value;
+var items = document.getElementsByClassName('item');
+let blackColor = "rgba(0, 0, 0, 0.8)";
+
+
 input.addEventListener("input", (event) => {
     value.textContent = event.target.value;
+    console.log(value.textContent);
+    totalNumberOfBoxes = event.target.value * event.target.value;
+    childGrid.style.gridTemplateColumns = `repeat(${value.textContent}, 1fr)`;
+    childGrid.innerText = '';
+    generateGrid();
 });
 
 /* creating divs for grid */
-
-for (let i = 0 ; i < 256 ; i ++){
+function generateGrid(){
     
-    var itemDiv = document.createElement('div');
-    itemDiv.setAttribute("class", "item");
-    childGrid.appendChild(itemDiv);
+    //generate the new grid//
+    for (let i = 0 ; i < totalNumberOfBoxes; i ++){
+        var itemDiv = document.createElement('div');
+        itemDiv.setAttribute("class", "item");
+        childGrid.appendChild(itemDiv);
+    }
+    
+    addHoverEvent();
+
 }
 
-var items = document.getElementsByClassName('item');
-let blackColor = "rgba(0, 0, 0, 0.8)";
-for (let i = 0 ; i < items.length ; i++) {
-    items[i].addEventListener('mouseenter', function() {
-        this.style.backgroundColor = blackColor;
-        this.style.border = `1px solid ${blackColor}`;
-    });
+function addHoverEvent (){
+    for (let i = 0 ; i < items.length ; i++) {
+        items[i].addEventListener('mouseenter', function() {
+            this.style.backgroundColor = blackColor;
+            this.style.border = `1px solid ${blackColor}`;
+        });
+    }    
 }
+
+//generate the initial grid//
+generateGrid();
+
 
